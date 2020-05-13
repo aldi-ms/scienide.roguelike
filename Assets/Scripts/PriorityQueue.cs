288af8df183b08da9c9a04c9e3462a2cf7434931
SCiENiDE00 <scienide00@hotmail.com>
2020-05-13 17:32:30 +0300

    Added PriorityQueue<T>

---
+++ Assets/Scripts/PriorityQueue.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace SCiENiDE.Core
+{
+    public class PriorityQueue<T> : IEnumerable<T> where T : class
+    {
+        private IComparer<T> _comparer;
+        private T[] _dataArray;
+        private int _firstEmptyIdx;
+
+        public PriorityQueue(Comparer<T> comparer, int capacity)
+        {
+            _comparer = comparer;
+            _dataArray = new T[capacity];
+            _firstEmptyIdx = 0;
+        }
+
+        public void Push(T value)
+        {
+            _dataArray[_firstEmptyIdx] = value;
+            _firstEmptyIdx++;
+            Array.Sort(_dataArray, _comparer);
+        }
+
+        public T Pop()
+        {
+            T returnValue = _dataArray[0];
+            _dataArray[0] = null;
+            Array.Sort(_dataArray, _comparer);
+
+            return returnValue;
+        }
+
+        public T Peek()
+        {
+            if (_dataArray.Length > 0)
+                return _dataArray[0];
+
+            return null;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new System.NotImplementedException();
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- Assets/Scripts/Testing.cs
+++ Assets/Scripts/Testing.cs
@@ -1,4 +1,6 @@
 ﻿using SCiENiDE.Core;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Testing : MonoBehaviour
